4e4729fa92c78496da97e58b0993e9985dc05a4f
MrYaros <molokan.yaroslavik@gmail.com>
2021-03-18 19:32:22 +0300

    GPIO

---
+++ gpio.py
@@ -0,0 +1,103 @@
+import RPi.GPIO as GPIO
+import time
+
+num_bits = 8
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+GPIO.setup(8, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+D = [24, 25, 8, 7, 12, 16, 20, 21]
+
+GPIO.output(D[:], 0)
+
+def lightUp(ledNumber, period):
+    GPIO.output(D[ledNumber], 1)
+    time.sleep(period)
+    GPIO.output(D[ledNumber], 0)
+
+def lightDown(ledNumber, period):
+    GPIO.output(D[ledNumber], 0)
+    time.sleep(period)
+    GPIO.output(D[ledNumber], 1)
+
+def blink(ledNumber, blinkCount, blinkPeriod):
+    for i in range(0,blinkCount):
+        GPIO.output(D[ledNumber], 1)
+        time.sleep(blinkPeriod)
+        GPIO.output(D[ledNumber], 0)
+        time.sleep(blinkPeriod)        
+
+def runningLight(count, period):
+    for j in range(0, count):
+        for i in range(0, num_bits):
+            lightUp(i % num_bits, period)
+
+def runningDark(count, period):
+    GPIO.output(D[:], 1)
+    for j in range(0, count):
+        for i in range(0, num_bits):
+            lightDown(i % num_bits, period)
+
+
+def decToBinList(decNumber):
+    decNumber = decNumber % 256
+    N = num_bits - 1
+    bits = []
+    while N > 0:
+        if(int(decNumber/(2**N)) == 1):
+            bits.append(1)
+            decNumber -= 2**N
+        else:
+            bits.append(0)
+        N -= 1
+    bits.append(decNumber)
+    return bits
+
+def lightNumber(number):
+    bits = decToBinList(number)
+    for i in range (0, num_bits):
+        GPIO.output(D[i], bits[num_bits - (i + 1)])
+
+def runningPattern(pattern, direction):
+    if(direction == -1):
+        while True:
+            lightNumber(pattern)
+            pattern = (pattern << 1) % 255
+            time.sleep(1)
+    if(direction == 1):
+        while True:
+            lightNumber(pattern)
+            pattern = (pattern >> 1) % 255
+            time.sleep(1)
+
+p = GPIO.PWM(D[3], 50)
+p.start(0)
+try:
+    while 1:
+        for dc in range(0, 101, 5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+        for dc in range(100, -1, -5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+p.stop()
+
+#runningPattern(15, -1)
+#lightUp(3, 2)
+#lightNumber(127)
+#time.sleep(2)
+#runningDark(2, 0.5)
+#blink(3, 3, 1)
+#runningLight(4, 0.5)
+
+
+GPIO.cleanup()

b1f84d90bcf04f2ead81499a282fb2e30e494df8
MrYaros <molokan.yaroslavik@gmail.com>
2021-04-01 16:44:45 +0300

    wav.py update

--- DAC/wav.py
+++ DAC/wav.py
@@ -10,11 +10,11 @@ print(f"Количество каналов = {data.shape[1]}")
 channels = data.shape[1]
 length = data.shape[0] / samplerate
 print(f"Длительность = {length}с")
-
+period = 1 / samplerate
 i = 0
-for j in range(0, channels, 1):
-	rest = length
-		while rest > 0:
-			num2dac(data[i], j)
-			i = i + 1
-			rest = rest - 1 / samplerate
+while length > 0:
+	for j in range(0, channels, 1):
+		num2dac(data[i], j)
+		i = i + 1
+		length = length - 1 / samplerate
+	time.sleep(period)

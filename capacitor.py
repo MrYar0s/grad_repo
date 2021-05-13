9ddca6b042689e189f193eb5fb38ed78dbc6799c
MrYar0s <70350421+MrYar0s@users.noreply.github.com>
2021-05-13 18:55:17 +0300

    Add files via upload

--- capacitor.py
+++ capacitor.py
@@ -25,7 +25,7 @@ GPIO.output(LEDS, 0)
 
 def decToBinList(decNumber):
 	decNumber = decNumber % 256
-	N = num_bits - 1
+	N = 7
 	bits = []
 	while N > 0:
 		if(int(decNumber/(2**N)) == 1):
@@ -37,98 +37,97 @@ def decToBinList(decNumber):
 	bits.append(decNumber)
 	return bits
 
-def num2leds(value):
+def num2pins(pins, value):
 	bits = decToBinList(value)
-	GPIO.output(LEDS, bits)
-
-def num2dac(value):
-	bits = decToBinList(value)
-	GPIO.output(D, bits)
+	GPIO.output(pins, bits)
 
 def adc():
 	#num of bits - 1
 	N = 7
 	#middle amount between 0 and 256
-	middle = 128
+	middle = 127
 	#status of the comparatop
 	#status = 1 -- value is higher
 	#status = 0 -- value is lower
-	status = GPIO.input(4)
 	while N > 0:
-		num2dac(middle)
-		time.sleep(0.001)
-		if !status:
+		num2pins(D,middle)
+		t.sleep(0.001)
+		if GPIO.input(4) == 0:
 			middle -= 2**(N - 1)
 		else:
 			middle += 2**(N - 1)
 		N -= 1
-	if !status:
+	if GPIO.input(4) == 0:
 		middle -= 1
 	else:
 		middle += 1
+	num2pins(LEDS,middle)
 	return middle
 
 def prepare():
 	#Turn off troykaModule supply
 	GPIO.output(17, 0)
-	while adc > 0:
+	while adc() > 0:
 		print('Wait! There is a discharge of the capacitor')
 		t.sleep(1)
 
 measure = []
 listT = []
-listV = []
 time_start = t.time()
 
 def charge():
 	GPIO.output(17, 1)
-	while adc < 254:
-		measure.append(adc)
+	while adc() < 240:
+		measure.append(adc())
 		listT.append(t.time() - time_start)
-		t.sleep(0.0001)
+		t.sleep(0.000001)
 
 def discharge():
 	GPIO.output(17, 0)
-	while adc > 0:
-		measure.append(adc)
+	while adc() > 5:
+		measure.append(adc())
 		listT.append(t.time() - time_start)
-		t.sleep(0.0001)
+		t.sleep(0.000001)
 
 def convertion():
 	num_of_elements = len(measure)
+	listV = [0 for i in range(0,num_of_elements)]
 	for i in range(0, num_of_elements):
-		listV[i] = (measure[i] * 3.3)/ 255
+		listV[i] = (measure[i] * 3.3)/255
+	return listV
 
-def makeplotV():
+def makeplotV(listV):
 	plt.plot(listT, listV)
 	plt.title('Процесс зарядки и разрядки конденсатора')
 	plt.xlabel('Время t, с')
 	plt.ylabel('Напряжение на конденсаторе U, В')
+	plt.show()
 
 def makeplotM():
 	plt.plot(listT, measure)
 	plt.title('Процесс зарядки и разрядки конденсатора')
+	plt.show()
 
-def makefile():
-	np.savetxt('data.txt', listV, fmt='%d')
+def makefile(listV):
+	np.savetxt('data.txt', measure, fmt='%d')
 	num_of_elements = len(listV)
 	sumV = 0
 	sumT = 0
-	for i in range(0, num_of_elements):
-		sumV += listV[i]
-		sumT += listT[i]
-	dT = sumT / num_of_elements
-	dV = sumV / num_of_elements
+	for i in range(0, num_of_elements - 1):
+		sumV += listV[i+1] - listV[i]
+		sumT += listT[i+1] - listT[i]
+	dT = sumT / len(listT)
+	dV = sumV / len(listV)
 	with open('settings.txt', 'w') as settings:
-		settings.write('dT = ' + repr(dT) + 'с' + '\n' + 'dV = ' + repr(dV) + 'В')
+		settings.write('dT = ' + repr(dT) + ' с' + '\n' + 'dV = ' + repr(dV) + ' В')
 		settings.close
 try:
 	prepare()
 	charge()
 	discharge()
-	convertion()
+	listV = convertion()
 	makeplotM()
-	makeplotV()
-	makefile()
+	makefile(listV)
+	makeplotV(listV)
 finally:
 	GPIO.cleanup()
